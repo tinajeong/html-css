f8fdfe2434b869bee73970fa293572b902d30830
Tina <migujeong@gmail.com>
2022-03-22 18:43:32 +0900

    try to fix CORS error

--- js/modal.js
+++ js/modal.js
@@ -8,58 +8,56 @@ const sidebar = document.querySelector(".sidebar");
 const container = document.querySelector(".container");
 
 function handleCloseclick() {
-    modalClass.classList.remove("is-visible");
-    sidebar.classList.remove("black");
+  modalClass.classList.remove("is-visible");
+  sidebar.classList.remove("black");
 }
 
-// function imageExists(image_url){
-
-//     fetch(image_url, { method: 'HEAD' })
-//     .then(res => {
-//         if (res.ok) {
-//             console.log('Image exists.');
-//             return true;
-//         } else {
-//             console.log('Image does not exist.');
-//             return false;
-//         }
-//     }).catch(err => console.log('Error:', err));
-
-//     return false;
-// }
-
-
 function imageExists(image_url) {
-    return true;
+  const headers = new Headers({
+    "Content-Type": "image/png",
+  });
+  console.log(image_url);
+  fetch(image_url, headers)
+    .then((res) => {
+      if (res.ok) {
+        console.log("Image exists.");
+        return true;
+      } else {
+        console.log("Image does not exist.");
+        return false;
+      }
+    })
+    .catch((err) => console.log("Error:", err));
+
+  return false;
 }
 
 function handleRegClick(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if(titleInput.value === "") {
-        alert("전시회 명을 입력하세요.");
-        return;
-    }
-    if(imageInput.value === "") {
-        alert("전시회 이미지 주소를 입력하세요.");
-        return;
-    }
-    else if(!imageExists(imageInput)) {
-        alert("유효한 이미지 주소가 아닙니다.");
-        return;
-    }
-    const a = document.createElement('a');
-    a.classList.add("bar-item");
-    a.innerText = titleInput.value;
-    a.href ="#";
-    sidebar.appendChild(a);
+  if (titleInput.value === "") {
+    alert("전시회 명을 입력하세요.");
+    return;
+  }
+  if (imageInput.value === "") {
+    alert("전시회 이미지 주소를 입력하세요.");
+    return;
+  } else if (!imageExists(imageInput.value)) {
+    alert("유효한 이미지 주소가 아닙니다.");
+    return;
+  }
+  const a = document.createElement("a");
+  a.classList.add("bar-item");
+  a.innerText = titleInput.value;
+  a.href = "#";
+  sidebar.appendChild(a);
 
-    const img = document.createElement('img');
-    img.alt = titleInput.value;
-    img.src = imageInput.value;
-    container.appendChild(img);
+  const img = document.createElement("img");
+  img.alt = titleInput.value;
+  img.src = imageInput.value;
+  container.appendChild(img);
 
-    handleCloseclick();
+  handleCloseclick();
 }
 closeModal.addEventListener("click", handleCloseclick);
 regModal.addEventListener("click", handleRegClick);
