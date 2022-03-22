0969e9aa11e2c4febf51a34e9977d4b4679fe16f
Tina <migujeong@gmail.com>
2022-03-22 12:33:58 +0900

    check user input of title and image

--- js/modal.js
+++ js/modal.js
@@ -15,6 +15,12 @@ function handleCloseclick() {
 function handleRegClick(event) {
     event.preventDefault();
 
+    if(titleInput.value === "") {
+        alert("전시회 명을 입력하세요.");
+    }
+    if(imageInput.value === "") {
+        alert("전시회 이미지 경로를 입력하세요.");
+    }
     const a = document.createElement('a');
     a.classList.add("bar-item");
     a.innerText = titleInput.value;
